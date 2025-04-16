e5a1fb55155c727954c93fe0c3a7861ba440edb8
Huzaif <huzaifnaeem29@gmail.com>
2025-04-16 09:39:38 +0500

    update some cahnges

--- chap7/writing_opeartions_file.py
+++ chap7/writing_opeartions_file.py
@@ -10,3 +10,9 @@ file.close()
 file_name = open("demo.txt", "a")
 inner_content = file_name.write("\ni want to become a rich")
 file_name.close()
+
+
+
+file_name = open("demo.txt", "a")
+inner_content = file_name.write("\ni want to become a rich")
+file_name.close()
