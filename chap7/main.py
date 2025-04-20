5cf71659bfe9f16be0bf0471621506e4c73c3adc
Huzaif <huzaifnaeem29@gmail.com>
2025-04-20 11:29:46 +0500

    commit chap 7

---
+++ chap7/main.py
@@ -0,0 +1,21 @@
+def admit_card(name, roll_num, center, time, day):
+    """ Generate Admit car throgh function"""
+    card = f"""
+========================
+GIAIC Admit Card
+========================
+Name: {name}
+Roll number: {roll_num}
+Center: {center}
+Time: {time}
+Day: {day}
+=======================
+Instructions:
+=======================
+1. Please arrives at least fifteen minute early
+2. Bring your ID card for verification
+"""
+    return card
+
+id_card = admit_card("Huzaif", 89470, "Onsite", "9am to 12pm", "Friday")
+print(id_card)
