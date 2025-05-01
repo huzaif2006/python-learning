afe6be52ddfc23dd686e00a1cd604b20d3dfc728
Huzaif <huzaifnaeem29@gmail.com>
2025-05-01 21:04:02 +0500

    commit chap 8

---
+++ chap8/exception_handling.py
@@ -0,0 +1,27 @@
+# num = input("enter a number: ")
+# print(f"multiplication table of {num} is down below")
+
+
+# try:
+#     for i in range(1, 11):
+#         print(f"{int(num)} x {i} = {int(num)*i}")
+# except:
+#     print("Invalid input")
+
+
+
+# print("program end")
+
+
+
+
+
+
+
+
+try:
+    num = int(input("enter a num: "))
+    List = ["apple", "mango", "banana"]
+    print(List[num])
+except
+ 
